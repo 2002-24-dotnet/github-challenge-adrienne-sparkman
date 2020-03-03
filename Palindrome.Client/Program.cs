bc392e59377997837aceaca410381ed09a0076e7
Adrienne Sparkman <adrienne.sparkman@gmail.com>
2020-03-03 10:53:51 -0600

    Initial Commit for Palindrome

---
+++ Palindrome.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Palindrome.Domain;
+
+namespace Palindrome.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic l = new Logic();
+            Console.WriteLine("Enter the string to check for palindrome");
+            string s = Console.ReadLine();
+            
+            Console.WriteLine(l.isPalindrome(s));
+        }
+    }
+}
---
+++ Palindrome.Domain/Logic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Palindrome.Domain
+{
+    public class Logic
+    {
+
+        public string simplify(string s)
+        {
+            s= s.ToLower();
+            s= s.Replace(" ", "");
+            s= s.Replace(".", "");
+            s= s.Replace("?", "");
+            s= s.Replace(",", "");
+            s= s.Replace("!", "");
+            return s;
+        }
+        public bool isPalindrome(string s)
+        {
+            s=simplify(s);
+            char[] letters = new char[s.Length];
+            for(int i=0; i<s.Length; i++)
+            {
+                letters[i] = s[s.Length-i-1];
+            }
+
+            string reverse = new string(letters);
+            return Equals(s,reverse);
+        }
+    }
+}
